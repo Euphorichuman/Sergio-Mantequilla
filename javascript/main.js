4d162f2c311a50a29c0eba1e97cac6cfe3a57be6
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-08-04 09:48:32 -0500

    [ADD]Index file, stylesheets and required Js functions

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
